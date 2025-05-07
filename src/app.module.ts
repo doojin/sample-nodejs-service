f120d84b68f22b9997a37bbf5976f3367a3842ac
doojin <dmitry.papka@gmail.com>
2025-05-07 20:20:46 -0300

    generating Profiles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [],
+  imports: [ProfilesModule],
   controllers: [],
   providers: [],
 })
