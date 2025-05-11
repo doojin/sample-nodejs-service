3eb02a020543677aba74df7addce9e005c963489
doojin <dmitry.papka@gmail.com>
2025-05-11 15:43:21 -0300

    adding health endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProfilesModule } from './profiles/profiles.module';
+import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ProfilesModule],
+  imports: [ProfilesModule, HealthModule],
   controllers: [],
   providers: [],
 })
