da4357a269185547a561a9ba94c26a9855eed2e8
doojin <dmitry.papka@gmail.com>
2025-05-12 17:09:47 -0300

    configuring database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { ProfilesModule } from './profiles/profiles.module';
 import { HealthModule } from './health/health.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ProfilesModule, HealthModule],
+  imports: [
+    ProfilesModule,
+    HealthModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_DATABASE'),
+        entities: [],
+        synchronize: false,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
